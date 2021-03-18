6e0db61e0dd16bca35c9ebf908b8e48e67432b04
kvs <victorkachalov@gmail.com>
2021-03-18 12:29:46 +0100

    spm adjustment

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftBloc",
-            path: "SwiftBloc/Classes"),
+            path: "Sources"),
     ]
 )
 
