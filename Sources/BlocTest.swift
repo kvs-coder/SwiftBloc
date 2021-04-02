0e83be5b37d76cf83ee1cfef655f2b6d4555f9ae
kvs <victorkachalov@gmail.com>
2021-04-02 18:54:39 +0200

    bloc test move to test

--- Sources/BlocTest.swift
+++ Sources/BlocTest.swift
@@ -8,31 +8,31 @@
 import Foundation
 import Combine
 
-final public class BlocTest<S: Equatable, B: Base<S>> {
-    public static func execute(
-        build: () -> B,
-        act: ((B) -> Void)?,
-        wait: TimeInterval? = 0,
-        expect: (() -> Any)?,
-        verify: ((Bool, String) -> Void)
-    ) {
-        var areEqual = false
-        var states = [S]()
-        let bloc = build()
-        let scheduler = ImmediateScheduler.shared
-        let cancellable = bloc.$state
-            .subscribe(on: scheduler)
-            .delay(for: .seconds(wait ?? 0), scheduler: scheduler)
-            .sink(receiveValue: { value in
-                states.append(value)
-            })
-        act?(bloc)
-        if expect != nil {
-            let expected = expect!()
-            areEqual = "\(states)" == "\(expected)"
-            let message = "State received: \(states). \nStates expected: \(expected)"
-            verify(areEqual, message)
-        }
-        cancellable.cancel()
-    }
-}
+//final public class BlocTest<S: Equatable, B: Base<S>> {
+//    public static func execute(
+//        build: () -> B,
+//        act: ((B) -> Void)?,
+//        wait: TimeInterval? = 0,
+//        expect: (() -> Any)?,
+//        verify: ((Bool, String) -> Void)
+//    ) {
+//        var areEqual = false
+//        var states = [S]()
+//        let bloc = build()
+//        let scheduler = ImmediateScheduler.shared
+//        let cancellable = bloc.$state
+//            .subscribe(on: scheduler)
+//            .delay(for: .seconds(wait ?? 0), scheduler: scheduler)
+//            .sink(receiveValue: { value in
+//                states.append(value)
+//            })
+//        act?(bloc)
+//        if expect != nil {
+//            let expected = expect!()
+//            areEqual = "\(states)" == "\(expected)"
+//            let message = "State received: \(states). \nStates expected: \(expected)"
+//            verify(areEqual, message)
+//        }
+//        cancellable.cancel()
+//    }
+//}
